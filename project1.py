d0c4c6f96b56929faee83b694ac9dc3a322b17d0
Ayush Gupta <ayushkgupta1998@gmail.com>
2019-12-06 02:26:36 +0530

    Updated

--- project1.py
+++ project1.py
@@ -1,24 +1,32 @@
-from apyori import apriori
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+from mlxtend.frequent_patterns import fpgrowth
 
 data_set = [['M', 'O', 'N', 'K', 'E', 'Y'], ['D', 'O', 'N', 'K', 'E', 'Y'], ['M', 'A', 'K', 'E'],
            ['M', 'U', 'C', 'K', 'Y'], ['C', 'O', 'O', 'K', 'I', 'E']]
-rule = apriori(data_set, min_support=0.6, min_confidence=0.8)
-rule_list = list(rule)
-print(f'Total Relations found: {len(rule_list)} with 60% minimum support and 80% minimum confidence')
 
-for item in rule_list:
-    pair = item[0]
-    items = [x for x in pair]
-    print(items)
-    print(f"Support: {str(item[1])}")
-    print(f"Confidence: {str(item[2][0][2])}")
-    print(f"Lift: {str(item[2][0][3])}")
-    for sub_item in item[2]:
-        x = [x for x in sub_item[0]]
-        if len(x) is 0:
-            continue
-        y = [y for y in sub_item[1]]
-        print(f'\tRelation: {str(x)} -> {str(y)}')
-        print(f'\tConfidence: {str(sub_item[2])}')
-        print(f'\tLift: {str(sub_item[3])}\n')
-    print("=====================================")
+
+def display(frame):
+    print(f'\tTotal Relations found: {frame.shape[0]} with 60% minimum support and 80% minimum confidence\n')
+    for i in range(frame.shape[0]):
+        print(f'\tRelation: \t{list(frame.antecedents[i])} -> {list(frame.consequents[i])}')
+        print(f'\tConfidence: {frame.confidence[i]}')
+        print(f'\tSupport: \t{frame.support[i]}')
+        print('\t--------------------------------')
+
+
+encoder = TransactionEncoder()
+fit_data_set = encoder.fit(data_set).transform(data_set)
+fit_data_set = pd.DataFrame(fit_data_set, columns=encoder.columns_)
+
+frame = fpgrowth(fit_data_set, use_colnames=True, min_support=0.6)
+frame = association_rules(frame, metric='confidence', min_threshold=0.8)
+print('Using FP-growth algorithm:')
+display(frame)
+
+frame = apriori(fit_data_set, use_colnames=True, min_support=0.6)
+frame = association_rules(frame, metric='confidence', min_threshold=0.8)
+print('Using Apriori algorithm:')
+display(frame)
